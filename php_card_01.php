0d14908d2a012dc802c8d085432e686e370d4df9
Cem Cansever <149318477+cem-cansever@users.noreply.github.com>
2023-11-08 18:14:29 +0300

    Update php_card_01.php

--- php_card_01.php
+++ php_card_01.php
@@ -4,6 +4,9 @@ $Kategori1="xxxx";
 $Kategori2="yyyy";
 $Kategori3="zzzz";
 $Kategori4="kkkk";
+
+$grt1="h_2.png";
+$grt2="h_3.png";
 ?>
 
 
@@ -21,115 +24,137 @@ $Kategori4="kkkk";
 <body>
 
 
-<div class="container bg-primary my-5">
-  
-  <div class="row">
 
-     <div class="col-sm-12 col-lg-3">
-      <ul class="list-group">
-        <li class="list-group-item"><?php echo $Kategori1 ?></li>
-        <li class="list-group-item"><?php echo $Kategori2 ?></li>
-        <li class="list-group-item"><?php echo $Kategori3 ?></li>
-        <li class="list-group-item"><?php echo $Kategori4 ?></li>
-     </ul>
-     </div>
+    <div class="container bg-primary my-5">
 
-
-     <div class="col-sm-12 col-lg-9">
-        
-        
-        <div class="card">
             <div class="row">
-            <div class="col">
-                
-                <img src="h_2.png" width="450" height="450">
-                
+
+                <div class="col-sm-12 col-lg-3 mt-3">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <?php echo $Kategori1 ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $Kategori2 ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $Kategori3 ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $Kategori4 ?>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="col-sm-12 col-lg-9 mt-3">
+
+
+                    <div class="card">
+                        <div class="row">
+                            <div class="col">
+
+                                <?php echo"<img src=\"{$grt1}\" width=\"450\" height=\"450\">" ?>
+
+
+                            </div>
+
+                            <div class="col">
+                                <div class="card-body">
+                                    <h5 class="card-title">Konu Başlığı</h5>
+                                    <p class="card-text">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dolores
+                                        eligendi! Eveniet, ab illo necessitatibus consectetur consequatur, reiciendis
+                                        veniam rem ipsum expedita possimus repudiandae animi qui nam assumenda, iure
+                                        harum.
+                                    </p>
+
+                                    <div>
+                                        <span class="badge bg-primary">105 Yorum</span>
+                                        <span class="badge bg-warning">105 Yorum</span>
+                                        <span class="badge bg-primary">105 Yorum</span>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
 
-            <div class="col">
-                <div class="card-body">
-                    <h5 class="card-title">Konu Başlığı</h5>
-                    <p class="card-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dolores eligendi! Eveniet, ab illo necessitatibus consectetur consequatur, reiciendis veniam rem ipsum expedita possimus repudiandae animi qui nam assumenda, iure harum.
-                    </p>
 
-                    <div>
-                        <span class="badge bg-primary">105 Yorum</span>
-                        <span class="badge bg-warning">105 Yorum</span>
-                        <span class="badge bg-primary">105 Yorum</span>
+            <div class="row">
+
+                <div class="col-sm-12 col-lg-3 mt-3">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <?php echo $Kategori1 ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $Kategori2 ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $Kategori3 ?>
+                        </li>
+                        <li class="list-group-item">
+                            <?php echo $Kategori4 ?>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="col-sm-12 col-lg-9 mt-3">
+
+
+                    <div class="card">
+                        <div class="row">
+                            <div class="col">
+
+                                <?php echo"<img src=\"{$grt2}\" width=\"450\" height=\"450\">" ?>
+
+                            </div>
+
+                            <div class="col">
+                                <div class="card-body">
+                                    <h5 class="card-title">Konu Başlığı</h5>
+                                    <p class="card-text">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dolores
+                                        eligendi! Eveniet, ab illo necessitatibus consectetur consequatur, reiciendis
+                                        veniam rem ipsum expedita possimus repudiandae animi qui nam assumenda, iure
+                                        harum.
+                                    </p>
+
+                                    <div>
+                                        <span class="badge bg-primary">105 Yorum</span>
+                                        <span class="badge bg-warning">105 Yorum</span>
+                                        <span class="badge bg-primary">105 Yorum</span>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
 
                     </div>
 
                 </div>
 
             </div>
-            </div>
 
         </div>
-        
-        
-    
-    </div>
-     
-
-    </div>
-  
-
-  <div class="row my-5">
-
-<div class="col-sm-12 col-lg-3">
- <ul class="list-group">
-   <li class="list-group-item"><?php echo $Kategori1 ?></li>
-   <li class="list-group-item"><?php echo $Kategori2 ?></li>
-   <li class="list-group-item"><?php echo $Kategori3 ?></li>
-   <li class="list-group-item"><?php echo $Kategori4 ?></li>
-</ul>
-</div>
 
 
-<div class="col-sm-12 col-lg-9">
-   
-   
-   <div class="card">
-       <div class="row">
-       <div class="col">
-           
-           <img src="h_2.png" width="450" height="450">
-           
-       </div>
-
-       <div class="col">
-           <div class="card-body">
-               <h5 class="card-title">Konu Başlığı</h5>
-               <p class="card-text">
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dolores eligendi! Eveniet, ab illo necessitatibus consectetur consequatur, reiciendis veniam rem ipsum expedita possimus repudiandae animi qui nam assumenda, iure harum.
-               </p>
-
-               <div>
-                   <span class="badge bg-primary">105 Yorum</span>
-                   <span class="badge bg-warning">105 Yorum</span>
-                   <span class="badge bg-primary">105 Yorum</span>
-
-               </div>
-
-           </div>
-
-       </div>
-       </div>
-
-   </div>
-   
-   
-</div>
-
-
-</div>
-
-
-</div>
-
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"></script>
+    </body>
 </html>
